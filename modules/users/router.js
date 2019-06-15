06e0cf1e97adf3da253c0d0c849879cc29f9c978
Konstantin Aleksandrov <alexandrovkm@gmail.com>
2019-06-15 12:17:50 +0700

    Add signin & local strategy

--- modules/users/router.js
+++ modules/users/router.js
@@ -1,6 +1,10 @@
-const Router  = require('koa-router');
-const User    = require('./models/user');
-const Message = require('./models/message');
+const Router    = require('koa-router');
+const User      = require('./models/user');
+const Message   = require('./models/message');
+// const checkAuth = require('./middlewares/authLocal');
+const passport  = require('koa-passport');
+
+passport.use('local', require('./strategies/local'));
 
 const apiRouter = new Router({
     prefix: '/api/v1'
@@ -15,12 +19,26 @@ apiRouter.get('/users', async ctx => {
     ctx.body = await User.find();
 });
 
-apiRouter.post('/users', async ctx => {
+apiRouter.post('/signup', async ctx => {
     const user = new User(ctx.request.body);
 
     await user.save();
 
     ctx.redirect('/');
+});
+
+apiRouter.post('/signin', async ctx => {
+    await passport.authenticate('local', async (err, user, info, status) => {
+        if (err) {
+            return ctx.throw(err);
+        }
+
+        if (!user) {
+            return ctx.throw(500, info);
+        }
+
+        ctx.body = 'Привет ' + user.name;
+    })(ctx);
 });
 
 apiRouter.get('/messages', async ctx => {
@@ -32,19 +50,7 @@ apiRouter.get('/messages', async ctx => {
     }
 });
 
-apiRouter.post('/messages', async ctx => {
-    const { email, password } = ctx.request.body;
-
-    const user = await User.findOne({ email: String(email) });
-
-    if (!user) {
-        return ctx.throw(400, 'пользователь не найден');
-    }
-
-    if (!user.checkPassword(password)) {
-        return ctx.throw(400, 'пароль не правильный');
-    }
-
+apiRouter.post('/messages', /*checkAuth,*/ async ctx => {
     const message = new Message({
         message: ctx.request.body.message,
         user_id: user._id
@@ -54,6 +60,9 @@ apiRouter.post('/messages', async ctx => {
 
     ctx.redirect('/');
 });
+
+
+
 
 module.exports = [
     apiRouter
