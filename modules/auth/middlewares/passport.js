9fd593a5fe0fe84ce5bf0fee521c2c19883c45eb
Konstantin Aleksandrov <alexandrovkm@gmail.com>
2019-06-20 20:32:41 +0700

    Remove modules

--- modules/auth/middlewares/passport.js
+++
@@ -1,7 +0,0 @@
-const passport = require('koa-passport');
-
-passport.use('local', require('../strategies/local'));
-passport.use('jwt', require('../strategies/jwt'));
-
-
-module.exports = passport;
--- modules/users/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('./router');
-
-module.exports = function (app) {
-    router.forEach(router => {
-        app.use(router.routes());
-    });
-};
