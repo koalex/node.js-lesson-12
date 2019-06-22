3f0169e3ad4e579eef2fd8de235e115a80002ff6
Konstantin Aleksandrov <alexandrovkm@gmail.com>
2019-06-22 10:28:40 +0700

    Move server to separate repo

--- middlewares/bodyparser.js
+++
@@ -1,9 +0,0 @@
-const bodyParser = require('koa-bodyparser');
-
-module.exports = async (ctx, next) => {
-    await bodyParser({ // application/x-www-form-urlencoded , application/json ONLY
-        formLimit: '1mb',
-        jsonLimit: '1mb',
-        textLimit: '1mb'
-    })(ctx, next);
-};
