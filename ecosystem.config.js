b44259cfb08f3b9afabd7e9e698b1405781d6090
Alexander Uspensky <walrus.north@gmail.com>
2019-07-01 16:56:16 +0700

    Add test deploy from "w-node"

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -42,6 +42,22 @@ module.exports = {
                 'cp /home/node/.env /home/node/app/current/.env && ' +
                 'npm install && git submodule init && git submodule update --remote && ' +
                 'npm run bootstrap && pm2 startOrReload ecosystem.config.js --env production'
+        },
+        wnode_test: {
+            env: {
+                NODE_ENV: 'production',
+            },
+            user: process.env.SSH_USER,
+            host: [process.env.SSH_HOST],
+            ssh_options: ['StrictHostKeyChecking=no'],
+            repo: 'https://github.com/w-node/node.js-lesson-12.git',
+            ref: 'w-node/test_artillery',
+            path: process.env.PM2_PATH,
+            // 'post-setup': '',
+            'post-deploy': 'export NVM_DIR=/root/.nvm && [ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh &&' +
+                'cp /home/node/.env /home/node/app/current/.env && ' +
+                'npm install && git submodule init && git submodule update --remote && ' +
+                'npm run bootstrap && pm2 startOrReload ecosystem.config.js --env production'
         }
     }
 };
